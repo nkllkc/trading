d22d9d3b2337a3302e1e41a1912c9d4f26c0f7a9
Nikola Lukic <nikolalukic@Nikolas-MacBook-Pro.local>
2023-06-04 00:03:07 +0100

    Fixup

--- yf.py
+++ yf.py
@@ -1,3 +1,38 @@
 import yfinance as yf
-msft = yf.Ticker("MSFT")
-print(msft.info)
+
+# CREATE A TICKER INSTANCE PASSING TESLA AS THE TARGET COMPANY
+tsla = yf.Ticker('TSLA')
+
+# CALL THE MULTIPLE FUNCTIONS AVAILABLE AND STORE THEM IN VARIABLES.
+actions = tsla.get_actions()
+analysis = tsla.get_analysis()
+balance = tsla.get_balance_sheet()
+calendar = tsla.get_calendar()
+cf = tsla.get_cashflow()
+info = tsla.get_info()
+inst_holders = tsla.get_institutional_holders()
+news = tsla.get_news()
+recommendations = tsla.get_recommendations()
+sustainability = tsla.get_sustainability()
+
+# PRINT THE RESULTS
+print(actions)
+print('*'*20)
+print(analysis)
+print('*'*20)
+print(balance)
+print('*'*20)
+print(calendar)
+print('*'*20)
+print(cf)
+print('*'*20)
+print(info)
+print('*'*20)
+print(inst_holders)
+print('*'*20)
+print(news)
+print('*'*20)
+print(recommendations)
+print('*'*20)
+print(sustainability)
+print('*'*20)
